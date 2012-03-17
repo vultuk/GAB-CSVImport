c69ef981daf6ad0d29f7773e7562ceea2889adc2
Simon Skinner <simonskinner@dk101.net>
2012-03-17 21:45:34 +0000

    Working CSV MUXer

--- class.csv.php
+++ class.csv.php
@@ -6,49 +6,237 @@
 		var $_data;
 		var $_dataLoaded;
 		var $_numberColumns;
+		var $_nameColumns;
+		
+		var $numberOfRows;
 		
 		
-		function checkNumberValid($number)
-		{
+		private function checkUKTelephone(&$strTelephoneNumber, &$intError, &$strError) {
+		  
+		  // Copy the parameter and strip out the spaces
+		  $strTelephoneNumberCopy = str_replace (' ', '', $strTelephoneNumber);
+		
+		  // Convert into a string and check that we were provided with something
+		  if (empty($strTelephoneNumberCopy)) {
+		    $intError = 1;
+		    $strError = 'Telephone number not provided';
+		    return false;
+		  }
+		  
+		  // Don't allow country codes to be included (assumes a leading "+") 
+		  if (preg_match('/^(\+)[\s]*(.*)$/',$strTelephoneNumberCopy)) {
+		    $intError = 2;
+		    $strError = 'UK telephone number without the country code, please';
+		    return false;
+		  }
+		  
+		  // Remove hyphens - they are not part of a telephone number
+		  $strTelephoneNumberCopy = str_replace ('-', '', $strTelephoneNumberCopy);
+		  
+		  // Now check that all the characters are digits
+		  if (!preg_match('/^[0-9]{10,11}$/',$strTelephoneNumberCopy)) {
+		    $intError = 3;
+		    $strError = 'UK telephone numbers should contain 10 or 11 digits';
+		    return false;
+		  }
+		  
+		  // Now check that the first digit is 0
+		  if (!preg_match('/^0[0-9]{9,10}$/',$strTelephoneNumberCopy)) {
+		    $intError = 4;
+		    $strError = 'The telephone number should start with a 0';
+		    return false;
+		  }		
+		  
+		  // Check the string against the numbers allocated for dramas
+		  
+		  // Expression for numbers allocated to dramas
 			
-			preg_match('^\s*\(?(020[7,8]{1}\)?[ ]?[1-9]{1}[0-9{2}[ ]?[0-9]{4})|(0[1-8]{1}[0-9]{3}\)?[ ]?[1-9]{1}[0-9]{2}[ ]?[0-9]{3})\s^', $number, $matches);
+			$tnexp[0] =  '/^(0113|0114|0115|0116|0117|0118|0121|0131|0141|0151|0161)(4960)[0-9]{3}$/';
+			$tnexp[1] =  '/^02079460[0-9]{3}$/';
+			$tnexp[2] =  '/^01914980[0-9]{3}$/';
+			$tnexp[3] =  '/^02890180[0-9]{3}$/';
+			$tnexp[4] =  '/^02920180[0-9]{3}$/';
+			$tnexp[5] =  '/^01632960[0-9]{3}$/';
+			$tnexp[6] =  '/^07700900[0-9]{3}$/';
+			$tnexp[7] =  '/^08081570[0-9]{3}$/';
+			$tnexp[8] =  '/^09098790[0-9]{3}$/';
+			$tnexp[9] =  '/^03069990[0-9]{3}$/';
 			
-			print_r($matches);
-			
-			if ( substr($number, 0, 1) == "0" ) {
-				$number = substr($number, 1);
-			}
-			
-			
-			
-			return $number;
+		  foreach ($tnexp as $regexp) {  
+		    if (preg_match($regexp,$strTelephoneNumberCopy, $matches)) {
+		      $intError = 5;
+		      $strError = 'The telephone number is either invalid or inappropriate';
+		      return false;
+		    }
+		  }
+		  
+		  // Finally, check that the telephone number is appropriate.
+		  if (!preg_match('/^(01|02|03|05|070|071|072|073|074|075|07624|077|078|079)[0-9]+$/',$strTelephoneNumberCopy)) {
+		    $intError = 5;
+		    $strError = 'The telephone number is either invalid or inappropriate';
+		    return false;
+		  }
+		  
+		  // Seems to be valid - return the stripped telephone number
+		  $strTelephoneNumber = $strTelephoneNumberCopy;
+		  $intError = 0;
+		  $strError = '';
+		  return true;  
 		}
 		
-		function getAltNumber($row)
+		
+		
+		/**
+		 * Function to check a telephone number is a valid UK number.
+		 * 
+		 * @access private
+		 * @param mixed $number
+		 * @param string $numberType (default: "ALL")
+		 * @return void
+		 */
+		private function checkNumberValid($number, $numberType="ALL")
 		{
-			$altNumber = $this->checkNumberValid($this->_data[$row][$this->_numberColumns[1]]);
+		
+			// Create an array to hold our valid digits
+			$valid = Array();
 			
-			if ($altNumber != FALSE && ( $this->getMainNumber($row) != $altNumber ) ) {
-				return $altNumber;
+			// Valid UK telephone number digits
+			$mobileArray = Array(7);
+			$landlineArray = Array(1,2);
+			
+			// Strip spaces from the number
+			$thisNumber = (int)str_replace(" ","",$number);
+			
+			// Make sure the number starts with a 0
+			if ( substr($thisNumber, 0, 1) != "0" ) {
+				$thisNumber = "0".$thisNumber;
+			}
+			
+			// Create a selection of valid digits depending on the type required
+			if ($numberType == "ALL") {
+				$valid = array_merge($mobileArray, $landlineArray);
+			} else if ($numberType == "MOBILE") {
+				$valid = $mobileArray;
+			} else if ($numberType == "LANDLINE") {
+				$valid = $landlineArray;
+			}
+
+			// Get the second digit from the number
+			$secondDi = (int)substr($thisNumber, 1, 1);
+			
+			// Check that the phone number is in the range we require
+			if ( in_array($secondDi, $valid) ) {
+			
+				// Run the function to check if it's a valid UK number
+				if (!$this->checkUKTelephone ($thisNumber, $errorNo, $errorText) ) {
+					return FALSE;
+				} else {
+					// If the number is valid then strip the 0 again and return it
+					if (strlen($thisNumber) > 9) {
+						if ( substr($thisNumber, 0, 1) == "0" ) {
+							$thisNumber = substr($thisNumber, 1);
+						}
+						return (int)$thisNumber;
+					} else {
+						return FALSE;
+					}
+				}
 			} else {
 				return FALSE;
 			}
 		}
 		
-		function getMainNumber($row)
+		/**
+		 * Pull the Alternate number from the CSV file.
+		 * 
+		 * @access public
+		 * @param mixed $row
+		 * @param mixed $mobileOnly (default: FALSE)
+		 * @return void
+		 */
+		public function getAltNumber($row, $mobileOnly=FALSE)
 		{
-			$mainNumber = $this->checkNumberValid($this->_data[$row][$this->_numberColumns[0]]);
+			if ($this->_numberColumns[1] != "x") {
+				$altNumber = $this->checkNumberValid($this->_data[$row][$this->_numberColumns[1]], $mobileOnly);
+				
+				if ($altNumber != FALSE AND $this->getMainNumber($row, $mobileOnly) != $altNumber ) {
+					return (int)$altNumber;
+				} else {
+					return FALSE;
+				}
+			} else {
+				return FALSE;
+			}
+		}
+		
+		
+		/**
+		 * Pull the Main number from the CSV file.
+		 * 
+		 * @access public
+		 * @param mixed $row
+		 * @param mixed $mobileOnly (default: FALSE)
+		 * @return void
+		 */
+		public function getMainNumber($row, $mobileOnly=FALSE)
+		{
+			$mainNumber = $this->checkNumberValid($this->_data[$row][$this->_numberColumns[0]], $mobileOnly);
 			
 			if ( $mainNumber != FALSE ) {
-				return $mainNumber;
+				return (int)$mainNumber;
+
 			} else {
-				$mainNumber = $this->checkNumberValid($this->_data[$row][$this->_numberColumns[1]]);
+				$mainNumber = $this->checkNumberValid($this->_data[$row][$this->_numberColumns[1]], $mobileOnly);
 				if ( $mainNumber != FALSE ) {
-					return $mainNumber;
+					return (int)$mainNumber;
 				} else {
 					return FALSE;
 				}
 			}
+		}
+		
+		
+		/**
+		 * Pull the First Name from the CSV file.
+		 * 
+		 * @access public
+		 * @param mixed $row
+		 * @return void
+		 */
+		public function getFirstName($row) {
+		
+			$firstName = $this->getColumn($row,$this->_nameColumns['fname']);
+			if ( !empty($firstName) ) {
+				return $firstName;
+			} else {
+				return FALSE;
+			}
+				
+		}
+		
+		
+		/**
+		 * Pull the Surname from the CSV file.
+		 * 
+		 * @access public
+		 * @param mixed $row
+		 * @return void
+		 */
+		public function getSurname($row) {
+			if ($this->_nameColumns['sname'] != "x") {
+				$surname = $this->getColumn($row,$this->_nameColumns['sname']);
+				if ( !empty($surname) ) {
+					return $surname;
+				} else {
+					return FALSE;
+				}
+			} else {
+				return FALSE;
+			}		
+		}
+		
+		function getColumn($row, $col) {
+			return $this->_data[$row][$col];
 		}
 		
 		// Set the columns the numbers are stored in.
@@ -62,27 +250,54 @@
 			
 		}
 		
+		function setNameColumns($fname, $sname) {
+			if ($this->_dataLoaded) {
+				$this->_nameColumns = array("fname"=>$fname, "sname"=>$sname);
+			} else {
+				return FALSE;
+			}
+		}
+		
+		
+		function setHeadings($newHeadings) {
+			// If we change the headings then we presume that row 1 is data
+			
+			array_unshift($this->_data, $newHeadings);
+			
+		}
+		
+		
+		function getFileHeadings() {
+			$headings = Array();
+			
+			return $this->_data[0];
+			
+		}
+		
+		
+		function countRows(){
+			return $this->numberOfRows;
+		}
 		
 		// Import a CSV from a given database ID
 		function load($id) {
 			
-			$db = Database::connect();
-			$results = $db->prepared_query(FALSE, 'SELECT file FROM files WHERE id=?;', "i", Array($id) );
+			$fd = fopen ($id, "r");
+			$contents = fread ($fd, filesize($id));
+			fclose ($fd);
 			
-			// If we've loaded data then we put it into the data
-			if (count($results > 0)) {
-				$this->_dataLoaded = TRUE;
-				
-				foreach(explode("\r\n", $results[0]['file']) as $line) {
-					$this->_data[] = explode(",", $line);
-				}
-				
-				return TRUE;
-				
-			} else {
-				$this->_dataLoaded = FALSE;
-				return FALSE;
+			$this->_dataLoaded = TRUE;
+			
+			$c = 0;
+			foreach(explode("\r\n", $contents) as $line) {
+				$this->_data[] = explode(",", $line);
+				$c++;
 			}
+			
+			$this->numberOfRows = $c;
+			
+			return TRUE;
+						
 		}
 		
 		// Construct function
--- index.php
+++ index.php
@@ -1,34 +1,326 @@
 <?php
 
-	error_reporting(E_ALL);
+	system("clear");
 
+	$time = microtime(); 
+	$time = explode(" ", $time); 
+	$time = $time[1] + $time[0]; 
+	$start = $time; 
+
+	error_reporting(0);
+	ini_set('memory_limit','2048M');
+	
 	require_once('class.database.php');
 	require_once('class.csv.php');
 	
-	$csvFile = new csv();
+	
+	$options = getopt("s:f:y:z:m:a:t:");
+	
+	
+	function runConversionScript($incReportType="F", $incFilename="F", $incMainNumber="F", $incAltNumber="F", $incFirstName="F", $incSurname="F") {
+		GLOBAL $options, $start, $csvFile;
+	
+		if ($incFilename == "F") $incFilename = $options['f'];
+		if ($incMainNumber == "F") $incMainNumber = $options['m'];
+		if ($incAltNumber == "F") $incAltNumber=$options['a'];
+		if ($incFirstName == "F") $incFirstName=$options['y'];
+		if ($incSurname == "F") $incSurname=$options['z'];
+	
+
+		
+		$mobileOnly = $incReportType;
+		$ext = ".".substr(strrchr($incFilename,'.'),1);
+		$filename = str_replace($ext,"",$incFilename);
+		
+		print "Loading Leads\n";
+		
+		if (!isset($csvFile)) {
+			$csvFile = new csv();
+			$loadFile = $csvFile->load($filename.$ext);
+		} else {
+			$loadFile = TRUE;
+		}
+		
+		if (!$loadFile) {
+			print "No file found";
+		} else {
+			$csvFile->setNumberColumns( Array($incMainNumber,$incAltNumber) );
+			$csvFile->setNameColumns( $incFirstName, $incSurname );
+			
+			
+			
+			$fh = fopen($filename."_".$mobileOnly.".txt", 'w') or die("can't open file");
+			
+			print "Writing ".$count." Leads\n\n";
+			
+			$OutputReport = "";
+			
+			$MissingNamesCount = 0;
+			$MissingNumbersCount = 0;
+			
+			$validNumbers = 0;
+			
+			$headings = $csvFile->getFileHeadings();
+			
+			unset($headings[(int)$incMainNumber]);
+			unset($headings[(int)$incAltNumber]);
+			
+			unset($headings[(int)$incSurname]);
+			unset($headings[(int)$incFirstName]);
+			
+			
+			$headingString = "";
+			foreach($headings AS $heading)
+			{
+				$headingString .= $heading."\t";
+			}
+			
+			$headingString .= "GAB First Name\tGAB Surname\t";
+			$headingString .= "GAB Main Number\tGAB Alt Number";
+			
+			$OutputReport .= $headingString."\tReason\n";
+			
+			fwrite($fh, $headingString."\n");
+			
+			$count = $csvFile->countRows();
+			
+			for ($i = 1; $i <= $count; $i++) {
+	    		
+	    		$AddLine = True;
+	    		$reason = "";
+	    		$singleLine = "";
+	    		
+	    		
+	    		$firstName = $csvFile->getFirstName($i);
+	    		$surname = $csvFile->getSurname($i);
+	    		
+	    		$mainNumber = $csvFile->getMainNumber($i, $mobileOnly);
+	    		$altNumber = $csvFile->getAltNumber($i, $mobileOnly);
+	    		
+	    		foreach ($headings AS $key => $heading) {
+	    			$singleLine .= $csvFile->getColumn($i, $key)."\t";
+	    		}
+	    		
+	    		
+	    		
+	    		if ( !$mainNumber AND !$altNumber ) {
+	    			// Check that we have at least 1 numbers
+	    			$AddLine = FALSE;
+	    			$reason = "No valid phone numbers available";
+	    			$MissingNumbersCount++;
+	    		} else if ( !$firstName AND !$surname ) {
+	    			// Check that we have at least 1 name
+	    			$AddLine = FALSE;
+	    			$reason = "No name available";
+	    			$MissingNamesCount++;
+	    		}
+	    		
+	    		
+	    		if ($AddLine) {
+	    			$validNumbers++;
+	    			$singleLine .= $firstName."\t".$surname."\t";
+	    			$singleLine .= $mainNumber."\t".$altNumber;
+	    			fwrite($fh, $singleLine."\n");
+	  				print "-";
+	  			} else {
+	  				$singleLine .= $csvFile->getColumn($i, (int)$incFirstName)."\t".$csvFile->getColumn($i, (int)$incSurname)."\t";
+	  				$singleLine .= $csvFile->getColumn($i, (int)$incMainNumber)."\t".$csvFile->getColumn($i, (int)$incAltNumber);
+	  				$OutputReport .= $singleLine."\t".$reason."\n";
+	  				print "!";
+	  			}
+	    		
+	
+	    		if (($i % 100) == 0) {
+	    			print " - ".$i."\n";
+	    		}
+	    				
+			}
+			
+			fclose($fh);
+			
+			print "\n\nAll Done!\n\n";
+			
+			
+			$time = microtime(); 
+			$time = explode(" ", $time); 
+			$time = $time[1] + $time[0]; 
+			$finish = $time; 
+			$totaltime = ($finish - $start); 
+			
+			
+			print "Processing Time: ".$totaltime." seconds\n\n";
+			
+			print "Invalid Numbers: ".$MissingNumbersCount."\nInvalid Names: ".$MissingNamesCount."\n\nValid Leads: ".$validNumbers;
+			
+			print "\n\n";
+			
+			
+			$fh = fopen($filename."_".$mobileOnly."_ErrorReport.csv", 'w') or die("can't open file");
+			fwrite($fh, str_replace("\t",",",$OutputReport));
+			fclose($fh);
+			
+		}
+	
+	}
 	
 	
 	
-	if (!$csvFile->load(1)) {
-		print "No file found";
+	
+	
+	
+	if (count($options) < 3 AND isset($options['t'])) {
+		
+		if ($options['t'] == "headers") {
+		
+			$csvFile = new csv();
+			
+			$ext = ".".substr(strrchr($options['f'],'.'),1);
+			$filename = str_replace($ext,"",$options['f']);
+			
+			if (!$csvFile->load($filename.$ext)) {
+				print "No file found";
+			} else {
+			
+				print "Headers for this file are..\n\n";
+				
+				$headings = $csvFile->getFileHeadings();
+				
+				foreach ($headings AS $key => $head) {
+					print $key." : ".$head."\n";
+				}
+				
+				print "\n";
+			
+			}
+		
+		} else if ($options['t'] == "wizard") {
+			
+			$csvFile = new csv();
+			
+			$ext = ".".substr(strrchr($options['f'],'.'),1);
+			$filename = str_replace($ext,"",$options['f']);
+			
+			if (!$csvFile->load($filename.$ext)) {
+				print "No file found";
+			} else {
+			
+				
+				
+				$headings = $csvFile->getFileHeadings();
+				
+				
+				$headingsValid = FALSE;
+				$headingsChanged = FALSE;
+				
+				while (!$headingsValid) {
+					
+					system("clear");
+					print "\n";
+					
+					print "Headers for this file are..\n\n";
+					
+					foreach ($headings AS $key => $head) {
+						print $key." : ".$head."\n";
+					}
+					
+					print "\n";
+					
+					print "Are these headings correct? (Y/N) : ";
+					
+					$correctHeadings = fgets(STDIN);
+					
+					if (str_replace("\n","",strtoupper($correctHeadings)) == "Y") {
+						$headingsValid = TRUE;
+						if ($headingsChanged) {
+							$csvFile->setHeadings($headings);
+						}
+					} else {
+					
+						print "\n";
+						
+						$headings = $csvFile->getFileHeadings();
+						
+						foreach ($headings AS $key => $heading) {
+						
+							print "Title for column ".$key." : ";
+							$headings[$key] = str_replace("\n","",fgets(STDIN));
+						
+						}
+						
+						$headingsChanged = TRUE;
+					
+					
+					}
+					
+					
+					print "\n";
+
+				}
+				
+				
+			
+			}
+			
+			
+			
+
+			
+			print "Enter conversion type (all, both, mobile, landline) : ";
+			$entConType = fgets(STDIN);
+			
+			print "Enter Main Number column id : ";
+			$entMainCol = fgets(STDIN);
+			
+			print "Enter Alt Number column id : ";
+			$entAltCol = fgets(STDIN);
+			
+			print "Enter First Name column id : ";
+			$entFirstName = fgets(STDIN);
+			
+			print "Enter Surname column id : ";
+			$entSurname = fgets(STDIN);
+			
+			print "\n";
+			
+			
+			// $incReportType=FALSE, $incFilename=FALSE, $incMainNumber=FALSE, $incAltNumber=FALSE, $incFirstName=FALSE, $incSurname=FALSE
+
+
+
+			if (str_replace("\n","",strtoupper($entConType)) == "BOTH") {
+				runConversionScript(str_replace("\n","",strtoupper("mobile")), $options['f'], (int)$entMainCol, (int)$entAltCol, (string)str_replace("\n","",$entFirstName), (string)str_replace("\n","",$entSurname));
+				runConversionScript(str_replace("\n","",strtoupper("landline")), $options['f'], (int)$entMainCol, (int)$entAltCol, (string)str_replace("\n","",$entFirstName), (string)str_replace("\n","",$entSurname));
+			} else {
+				runConversionScript(str_replace("\n","",strtoupper($entConType)), $options['f'], (int)$entMainCol, (int)$entAltCol, (string)str_replace("\n","",$entFirstName), (string)str_replace("\n","",$entSurname));
+			}
+			
+		}
+		
+	} else if (count($options) < 3) {
+		// If there's no arguments then explain!
+		print "No arguments given\n\n";
+	
+		print "-s ABC          : Output sample (ALL, BOTH, MOBILE, LANDLINE)"."\n";
+		print "-f abc.csv      : Input filename"."\n";
+		print "-m x            : Main number column id"."\n";
+		print "-a x            : Alt number column id"."\n";
+		print "-y x            : First name column id"."\n";
+		print "-z x            : Surname column id"."\n";
+		
+		print "\n\n";
 	} else {
-		$csvFile->setNumberColumns( Array(2, 4) );
-		
-		print $csvFile->getMainNumber(1);
-		
-		print " - ";
-		
-		print $csvFile->getAltNumber(1);
+	
+		// $incReportType=FALSE, $incFilename=FALSE, $incMainNumber=FALSE, $incAltNumber=FALSE, $incFirstName=FALSE, $incSurname=FALSE
+	
+		runConversionScript();
 		
 	}
-
-
 	
 	
+	
+	
+	
+	
+		
 
 ?>
-
-
-
-
-
